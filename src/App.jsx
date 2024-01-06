ad59d44fa21ee52dc1dea3f01ba8f404c77f401f
Vladyslav Rokhmaniuk <botaniqyes@gmail.com>
2024-01-06 07:32:53 +0200

    - add projectState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import ProjectSidebar from "./components/ProjectSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import { useState } from "react";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: null,
+    projects: [],
+  });
+
+  const handleStartNewProject = () => {
+    setProjectState((prevState) => ({
+      ...prevState,
+      selectedProjectId: 0,
+    }));
+  };
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NoProjectSelected onStartAddProject={handleStartNewProject} />;
+  } else if (projectState.selectedProjectId === 0) {
+    content = <NewProject />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleStartNewProject} />
+      {content}
     </main>
   );
 }
